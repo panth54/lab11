5c800ea4c4bab2705d0d6352b68b4f65f63c272a
Phanthat Muenprap <phanthat54@gmail.com>
2021-12-16 17:30:24 +0700

    lab11_3 finish

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    
+
+    double sumAv, sumSD, average, SD;
+    int n=0;
+    while(getline(source, textline)){
+        sumSD += pow(stod(textline), 2);
+        sumAv += stod(textline);
+        n++;
+    }
+    
+    average = sumAv/n;
+    SD = sqrt((sumSD/n) - pow(average, 2));
+
+
+    cout << "Number of data = " <<n <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " <<average <<endl;
+    cout << "Standard deviation = " <<SD <<endl;
+
+    return 0;
+}
